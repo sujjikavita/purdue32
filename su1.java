74739e40ceb1b038c74bc9240aa4db6aaeee9faf
sujata <suja@dev.com>
2023-07-18 17:31:20 +0000

    testing rebase from dev1

--- su1.java
+++ su1.java
@@ -1,4 +1,8 @@
+
 testing conflicts
 this is from feature branch
 this is for testing from sujata to check conflicts from defect branch.
-testing rebase operation from defect branch.
+testing rebase operation from defect branch
+testing purpose
+
+testing rebase from feature branch with dev1.
